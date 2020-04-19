c9ae2795d3eda74b339d0797eb438509cf7712ef
LiteWorm <ly090820@gmail.com>
2020-04-19 15:47:44 +0800

    spring boot 配置yml

--- JAVA/spb/src/main/java/com/liteworm/spb/SpbApplication.java
+++ JAVA/spb/src/main/java/com/liteworm/spb/SpbApplication.java
@@ -2,9 +2,11 @@ package com.liteworm.spb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @RestController
---
+++ JAVA/spb/src/main/java/com/liteworm/spb/config/GlobalProperties.java
@@ -0,0 +1,18 @@
+package com.liteworm.spb.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName GlobalProperties
+ * @Decription @TOTO
+ * @AUthor LiteWorm
+ * @Date 2020/4/17 23:32
+ * @Version 1.0
+ **/
+@Component
+@ConfigurationProperties
+public class GlobalProperties {
+}
