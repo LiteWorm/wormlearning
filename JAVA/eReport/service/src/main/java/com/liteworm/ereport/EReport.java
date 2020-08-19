cb84c7993d883549ce6b4966092c334a5c364912
eadlon <eadlon@163.com>
2020-08-19 15:45:04 +0800

    调整依赖

---
+++ JAVA/eReport/service/src/main/java/com/liteworm/ereport/EReport.java
@@ -0,0 +1,22 @@
+package com.liteworm.ereport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+class EReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EReportApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
+
