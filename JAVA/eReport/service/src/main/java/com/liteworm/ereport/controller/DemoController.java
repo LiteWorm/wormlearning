9665f51c649e5d9b7651104d5c8a44b95b5ff8b7
eadlon <eadlon@163.com>
2020-08-19 16:39:54 +0800

    拆分controller 为独立模块

--- JAVA/eReport/service/src/main/java/com/liteworm/ereport/controller/DemoController.java
+++ JAVA/eReport/service/src/main/java/com/liteworm/ereport/controller/DemoController.java
@@ -1,4 +1,15 @@
 package com.liteworm.ereport.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        System.out.println("测试代码");
+        return String.format("Hello %s!", name);
+    }
 }
