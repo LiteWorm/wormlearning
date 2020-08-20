b70f92ad994e44e62fa9ebc38f911a0ab33d3667
eadlon <eadlon@163.com>
2020-08-20 13:58:05 +0800

    添加日志组件

--- JAVA/eReport/service/src/main/java/com/liteworm/ereport/controller/DemoController.java
+++ JAVA/eReport/service/src/main/java/com/liteworm/ereport/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.liteworm.ereport.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+    private static final Logger log = LoggerFactory.getLogger(DemoController.class);
+
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+        log.debug("测试debug");
+        log.error("测试error");
+        log.info("测试info");
         return String.format("Hello %s!", name);
     }
 }
--- JAVA/eReport/service/src/main/java/com/liteworm/ereport/controller/UserAccessController.java
+++ JAVA/eReport/service/src/main/java/com/liteworm/ereport/controller/UserAccessController.java
@@ -1,10 +1,14 @@
 package com.liteworm.ereport.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserAccessController {
+    private static final Logger log = LoggerFactory.getLogger(DemoController.class);
+
     @PostMapping("/login")
     public String userLogin(){
         String logResult = null;
