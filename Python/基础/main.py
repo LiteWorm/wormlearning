f72d8e7226ef025c25e79f0fa4fbeb25937157ae
eadlon <eadlon@163.com>
2021-05-26 23:44:50 +0800

    commit

--- Python/基础/main.py
+++ Python/基础/main.py
@@ -1,4 +1,18 @@
 # This is a sample Python script.
+import jieba
+import imageio
+import wordcloud
+import keras
+import pandas
+import scipy
+import sklearn
+import matplotlib
+import featuretools
+
+
+
+
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -8,13 +22,17 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+
 def print_ls():
     print('*******')
+
 
 def test_number():
     print(1+3)
 
 # Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_ls()
---
+++ Rlang/AI/main.R
@@ -0,0 +1,5 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: liuyong
+# Created on: 2021/4/24
+
