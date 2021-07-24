73d3a28dad62a71faf8fa42cfb9bd45501028179
eadlon <eadlon@163.com>
2021-07-24 14:02:17 +0800

    测试mybatis 自动生成代码

---
+++ JAVA/mybatis-generator/src/test/java/com/eadlon/mybatisgenerator/MybatisGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eadlon.mybatisgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
