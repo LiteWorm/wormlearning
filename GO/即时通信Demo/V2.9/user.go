beef29aa2895850276a88db8e2bab352632da935
eadlon <eadlon@163.com>
2025-05-20 18:21:32 +0800

    demo upadte

---
+++ GO/即时通信Demo/V2.9/user.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"net"
+	"strings"
+)
+
+type User struct {
+	Name   string
+	Addr   string
+	C      chan string
+	conn   net.Conn
+	server *Server
+}
+
+//创建用户
+func NewUser(conn net.Conn, server *Server) *User {
+	userAddr := conn.RemoteAddr().String()
+	user := &User{
+		Name:   userAddr,
+		Addr:   userAddr,
+		C:      make(chan string),
+		conn:   conn,
+		server: server,
+	}
+	//启动监听当前user channel的goroutine
+	go user.ListenMessage()
+	return user
+}
+
+//监听当前User channel，有新消息则发送到客户端
+func (this *User) ListenMessage() {
+	for {
+		msg := <-this.C
+		this.conn.Write([]byte(msg + "\n"))
+	}
+}
+
+//用户上线
+func (this *User) Online() {
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+	this.server.BroadCast(this, "上线了")
+}
+
+//用户下线
+func (this *User) Offline() {
+	this.server.mapLock.Lock()
+	delete(this.server.OnlineMap, this.Name)
+	this.server.mapLock.Unlock()
+	this.server.BroadCast(this, "下线了")
+}
+
+//向客户端输出消息
+func (this *User) sendMsg(msg string) {
+	this.conn.Write([]byte(msg))
+}
+
+//用户消息
+func (this *User) DoMessage(msg string) {
+	if msg == "who" {
+		this.server.mapLock.Lock()
+		for _, user := range this.server.OnlineMap {
+			olMsg := "[" + user.Addr + "]" + user.Name + ":在线...\n"
+			this.sendMsg(olMsg)
+		}
+		this.server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		newName := strings.Split(msg, "|")[1]
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			this.sendMsg("当前用户名已被使用\n")
+		} else {
+			this.server.mapLock.Lock()
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[newName] = this
+			this.server.mapLock.Unlock()
+			this.Name = newName
+			this.sendMsg("您已经更名为：" + newName + "\n")
+		}
+	} else {
+		this.server.BroadCast(this, msg)
+	}
+}
